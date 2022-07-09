a1dab777d2516a28efdc3e62670b6ecfa1260d42
Koralkov Artem <artemkoralkov@gmail.com>
2022-07-09 16:06:10 +0300

    feat: implement album schema

---
+++ src/modules/albums/albums.schema.ts
@@ -0,0 +1,44 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type Album {
+    id: ID!
+    name: String
+    released: Int
+    artists: [Artist]
+    bands: [Band]
+    tracks: [Track]
+    genres: [Genre]
+    image: String
+  }
+
+  input CreateAlbumInput {
+    name: String!
+    released: Int
+    artistsIds: [String]
+    bandsIds: [String]
+    tracksIds: [String]
+    genresIds: [String]
+    image: String
+  }
+  input UpdateAlbumInput {
+    name: String
+    released: Int
+    artistsIds: [String]
+    bandsIds: [String]
+    tracksIds: [String]
+    genresIds: [String]
+    image: String
+  }
+
+  type Mutation {
+    createAlbum(newAlbum: CreateAlbumInput!): Album!
+    updateAlbum(id: ID!, updatedAlbum: UpdateAlbumInput!): Album!
+    deleteAlbum(id: ID!): ID
+  }
+
+  type Query {
+    album(id: ID!): Album
+    albums: [Album]
+  }
+`;
