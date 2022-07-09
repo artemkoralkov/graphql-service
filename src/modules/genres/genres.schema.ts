7f7a4e79674e35b18731d7337b2014c56b9aff1a
Koralkov Artem <artemkoralkov@gmail.com>
2022-07-09 20:40:16 +0300

    feat: implement geners schema

---
+++ src/modules/genres/genres.schema.ts
@@ -0,0 +1,35 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type Genre {
+    id: ID!
+    name: String
+    description: String
+    country: String
+    year: Int
+  }
+
+  input CreateGenreInput {
+    name: String!
+    description: String
+    country: String
+    year: Int
+  }
+
+  type Query {
+    genre(id: ID!): Genre
+    genres: [Genre]
+  }
+  input UpdateGenreInput {
+    name: String
+    description: String
+    country: String
+    year: Int
+  }
+
+  type Mutation {
+    createGenre(newGenre: CreateGenreInput!): Genre
+    updateGenre(id: ID!, updatedGenre: UpdateGenreInput!): Genre
+    deleteGenre(id: ID!): String
+  }
+`;
